3893b36e4e9277736816ee39835e0545e07c11cb
adrnkaczmarek <adrn.kaczmarek@gmail.com>
2018-02-20 19:45:55 +0100

    refactor

--- master/bayes_master.py
+++ master/bayes_master.py
@@ -1,24 +1,20 @@
 import json
 import requests
+import time
 from requests.exceptions import ConnectionError
+
+
 data = json.load(open('file.json'))
-
-
-# 1. skrypt 1 csv -> json
-# 2. json -> klasy do klasyfikatora
-# 3. klasyfikacja bayesem
-#grupowac po '#' , tabele na podtabele
-
 tmp = 0
 
 for row in data["content"]:
     tmp += 1
 
-tab = []
-tab.append("172.21.0.2:5000")
-tab.append("172.21.0.3:5000")
-tab.append("172.21.0.4:5000")
-ilosc = len(tab)
+work_endpoints = []
+work_endpoints.append("172.21.0.2:5000")
+work_endpoints.append("172.21.0.3:5000")
+work_endpoints.append("172.21.0.4:5000")
+ilosc = len(work_endpoints)
 tmp2 = tmp / ilosc
 tmp = 0
 tmp3 = 0
@@ -29,11 +25,12 @@ def checkContainersAvailability():
 
     while areNotAvailable:
         try:
-            requests.get("http://" + tab[0] + "/ping")
-            requests.get("http://" + tab[1] + "/ping")
-            requests.get("http://" + tab[2] + "/ping")
+            requests.get("http://" + work_endpoints[0] + "/ping")
+            requests.get("http://" + work_endpoints[1] + "/ping")
+            requests.get("http://" + work_endpoints[2] + "/ping")
         except ConnectionError as e:
             print("Some workers are still unavailable")
+            time.sleep(1)
             continue
         areNotAvailable = False
 
@@ -44,13 +41,12 @@ checkContainersAvailability()
 
 
 for row in data["content"]:
-    r = requests.post('http://' + tab[tmp3] + '/add', json.dumps(row))
-#    print(r.text)
+    r = requests.post('http://' + work_endpoints[tmp3] + '/add', json.dumps(row))
     tmp3 += 1
 
     if tmp3 == ilosc:
         tmp3 = 0
         pliktmp = {}
 
-response = requests.get('http://' + tab[0] + '/test1')
+response = requests.get('http://' + work_endpoints[0] + '/test1')
 print("Workers are currently learned")
